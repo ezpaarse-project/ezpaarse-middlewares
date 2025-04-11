2888bf793089082ca4332a4a08c8f33355747c79
felixleo22 <leofelixpro22@gmail.com>
2025-04-11 07:50:37 +0200

    fix: enrich with only institutionName

--- abesid-to-etab/index.js
+++ abesid-to-etab/index.js
@@ -21,20 +21,9 @@ const parseCSVToJSON = (filePath) => {
     });
 
     parser.on('end', () => {
-      const data = results.reduce((acc, item) => {
-        acc[item['Identifiant Abes']] = {
-          ['Siren']: item['Siren'],
-          ['Nom de l\'etablissement']: item['Nom de l\'etablissement'],
-          ['Type de l\'etablissement']: item['Type de l\'etablissement'],
-          ['Adresse de l\'etablissement']: item['Adresse de l\'etablissement'],
-          ['Ville']: item['Ville'],
-          ['Telephone contact']: item['Telephone contact'],
-          ['Nom et prenom contact']: item['Nom et prenom contact'],
-          ['Adresse mail contact']: item['Adresse mail contact'],
-          ['IP validees']: item['IP validees'],
-        };
-        return acc;
-      }, {});
+      const data = Object.fromEntries(
+        results.map(item => [item['ID Etablissement'], item['Nom Etablissement']])
+      );
       resolve(data);
     });
 
@@ -45,36 +34,12 @@ const parseCSVToJSON = (filePath) => {
 };
 
 module.exports = function () {
-  const job = this.job;
-  const report = this.report;
   const req = this.request;
   const logger = this.logger;
 
   const sourceField = req.header('abesid-to-etab-source-field') || 'abes-id';
-  let enrichedFields = req.header('abesid-to-etab-enriched-fields');
+  const enrichedField = req.header('abesid-to-etab-enriched-field') || 'institutionName';
 
-
-  if (enrichedFields) {
-    try {
-      enrichedFields = JSON.parse(enrichedFields);
-    } catch (err) {
-      const error = new Error(`[abesid-to-etab]: Cannot parse enrichedFields ${err}`);
-      error.status = 500;
-      return error;
-    }
-  } else {
-    enrichedFields = {
-      'Siren': 'siren',
-      'Nom de l\'etablissement': 'institutionName',
-      'Type de l\'etablissement': 'institutionType',
-      'Adresse de l\'etablissement': 'institutionAddress',
-      'Ville': 'institutionCity',
-      'Telephone contact': 'institutionPhone',
-      'Nom et prenom contact': 'institutionContact',
-      'Adresse mail contact': 'institutionEmail',
-      'IP validees': 'institutionIpRange'
-    }
-  }
 
   let institutions = {};
 
@@ -98,14 +63,7 @@ module.exports = function () {
     if (!ec || !ec[sourceField]) { return next(); }
 
     if (institutions[ec[sourceField]]) {
-      const dataFromCSV = institutions[ec[sourceField]];
-
-      Object.entries(enrichedFields).forEach(([keyFromCSV, enrichedKey]) => {
-        if (ec[enrichedKey]) {
-          return;
-        }
-        ec[enrichedKey] = dataFromCSV[keyFromCSV];
-      });
+      ec[enrichedField] = institutions[ec[sourceField]];
     }
 
     next();
